cd4653a6f54f824a50f88553979f0a0ec778f587
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2018-05-14 12:23:44 +0200

    refactor HomePage

--- src/test/java/piswib/Utils.java
+++ src/test/java/piswib/Utils.java
@@ -2,6 +2,7 @@ package piswib;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import piswib.logged.HomePage;
 import piswib.login.LoginPage;
 
 import static piswib.Repository.GOOD_LOGIN;
@@ -9,6 +10,7 @@ import static piswib.Repository.GOOD_LOGIN;
 public class Utils {
     private WebDriver browser;
     private LoginPage login_page = PageFactory.initElements(browser, LoginPage.class);
+    private HomePage home_page =PageFactory.initElements(browser, HomePage.class);
 
     public Utils(WebDriver browser) {
         this.browser = browser;
@@ -18,7 +20,7 @@ public class Utils {
         login_page.sendCorrectLogin();
         login_page.sendCorrectPassword();
         login_page.submit();
-        return login_page.loginLabelText().contains(GOOD_LOGIN);
+        return home_page.loginLabelText().contains(GOOD_LOGIN);
     }
 
 }
