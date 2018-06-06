7bbb80424c4e67c5d031d603faf60a6c3ef1454e
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2018-06-06 09:30:51 +0200

    testlink

--- src/test/java/piswib/BasePage.java
+++ src/test/java/piswib/BasePage.java
@@ -14,8 +14,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public abstract class BasePage {
 
-    protected boolean isExist(int size){
-        if(size > 0){
+    protected boolean isExist(int size) {
+        if (size > 0) {
             return true;
         }
         return false;
@@ -32,36 +32,41 @@ public abstract class BasePage {
     }
 
     private void set_status_testlink(String external_id, boolean status) {
+        if (!System.getenv("LOGNAME").contains("d")) {
+            return;
+        }
+        ExecutionStatus execution_status = getExecutionStatus(status);
+        TestLinkAPI api = getTestLinkAPI();
+        TestPlan test_plan = api.getTestPlanByName("Ścieżki krytyczne", "PI SWIB");
+        TestCase caseTest = api.getTestCaseByExternalId(external_id, 1);
+        Build build = api.getLatestBuildForTestPlan(test_plan.getId());
+        api.setTestCaseExecutionResult(caseTest.getId(), null, test_plan.getId(), execution_status,
+                build.getId(), build.getName(), null, true, null, null, null, null, false);
 
+    }
 
-
-
-System.out.println(System.getenv());
-System.out.println(!System.getenv("LOGNAME").contains("d"));
-        if(!System.getenv("LOGNAME").contains("d")){return;}
+    private TestLinkAPI getTestLinkAPI() {
+        TestLinkAPI api = null;
         String url = "http://192.168.2.52/testlink193/lib/api/xmlrpc.php";
         String dev_key = System.getenv("TESTLINK_KEY");
-        ExecutionStatus execution_status;
-        TestLinkAPI api = null;
-        if (status) {
-            execution_status = ExecutionStatus.PASSED;
-        } else {
-            execution_status = ExecutionStatus.FAILED;
-        }
-
         try {
             api = new TestLinkAPI(new URL(url), dev_key);
         } catch (MalformedURLException e) {
             System.err.println("Brak dostępu do adresu: " + url);
         }
-        TestPlan test_plan = api.getTestPlanByName("Ścieżki krytyczne", "PI SWIB");
-        TestCase caseTest = api.getTestCaseByExternalId(external_id, 1);
+        return api;
+    }
 
-        Build build = api.getLatestBuildForTestPlan(test_plan.getId());
+    private ExecutionStatus getExecutionStatus(boolean status) {
+        ExecutionStatus execution_status;
+        if (status) {
+            execution_status = ExecutionStatus.PASSED;
+        } else {
+            execution_status = ExecutionStatus.FAILED;
+        }
+//        execution_status = ExecutionStatus.NOT_RUN;
 
-        api.setTestCaseExecutionResult(caseTest.getId(), null, test_plan.getId(), execution_status,
-                build.getId(), build.getName(), null, true, null, null, null, null, false);
-
+        return execution_status;
     }
 
 }
